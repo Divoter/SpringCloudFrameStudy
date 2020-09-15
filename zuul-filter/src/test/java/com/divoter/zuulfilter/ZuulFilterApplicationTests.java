236893d1281f4f1412aadcc45875d05b4337dfcf
divoter <1849268954@qq.com>
2020-09-15 21:49:18 +0800

    create project(zuul) : zuul-filter service  1st

---
+++ zuul-filter/src/test/java/com/divoter/zuulfilter/ZuulFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divoter.zuulfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
