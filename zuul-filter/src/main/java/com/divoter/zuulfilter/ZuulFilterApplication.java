4a06490a2cfaa19805674bc388ec94bbbf979200
divoter <1849268954@qq.com>
2020-09-17 17:26:45 +0800

    zuul-filter: fixed config-client、rabbitmq

--- zuul-filter/src/main/java/com/divoter/zuulfilter/ZuulFilterApplication.java
+++ zuul-filter/src/main/java/com/divoter/zuulfilter/ZuulFilterApplication.java
@@ -2,12 +2,11 @@ package com.divoter.zuulfilter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class ZuulFilterApplication {
 
